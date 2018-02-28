f8b8a5d97a2a12d785a60bcdcefce2ac8c6f9127
Claudio Luck <claudio.luck@pep.foundation>
2018-02-28 01:10:46 +0100

    add missing resources files
    --HG--
    branch : setup

--- setup.py
+++ setup.py
@@ -1,5 +1,5 @@
 
-from distutils.core import setup
+from setuptools import setup
 
 setup(
         name='YML2',
@@ -9,7 +9,10 @@ setup(
         author_email="vb@pep.foundation",
         packages=["yml2"],
         install_requires=['lxml'],
-        #scripts=['yml2/yml2proc', 'yml2/yml2c'],
+        package_data = {
+            '': ['gpl-2.0.txt'],
+            'yml2': ['*.css', '*.yml2', '*.yhtml2', '*.ysl2'],
+        },
         entry_points = {
             'console_scripts': [
                 'yml2c=yml2.yml2c:main',
